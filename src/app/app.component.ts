36f16978df7c9532b18c136a87e9f2f25f7fe5bb
Rafael Lima <rafaa99@gmail.com>
2022-08-30 19:13:42 -0300

    [FormGroup] create the register form in app.component.ts and naming form controls in the app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // Registrando o form como um FormGroup com seus controles
+  registerForm = new FormGroup({
+    // 'name' é um input que vai receber validações dentro do FormControl, assim como 'email' e 'password'
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
   constructor() {}
 }
