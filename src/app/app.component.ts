37b584ef0b841d00bfb913ae4d916777c051cee5
Rafael Lima <rafaa99@gmail.com>
2022-08-30 19:45:21 -0300

    [Form Validity] show form validations in View and adding a lot of validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,38 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // Para quando o formulário for enviado!
+  isFormSend: boolean = false;
+
   // Registrando o form como um FormGroup com seus controles
   registerForm = new FormGroup({
     // 'name' é um input que vai receber validações dentro do FormControl, assim como 'email' e 'password'
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    name: new FormControl(
+      'Valor inicial de um campo coloca aqui, sugiro deixar em branco',
+      // Validação para os campos do formulário devem ser colocados em Array ou em um objeto como mostrarei nos exemplos a seguir
+      [Validators.minLength(3), Validators.pattern(/^[a-z0-9]+$/)]
+    ),
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.minLength(8), Validators.maxLength(20)],
+    }),
   });
   constructor() {}
+
+  onSubmit() {
+    // Desestruturando validações do registerForm
+    const { name, email, password } = this.registerForm.value;
+    // Mostrando as validações no console
+    console.log({ name, email, password, form: this.registerForm });
+
+    // Mostrar envio do form!
+    this.isFormSend = true;
+
+    // Somente para resetar o envio do form já que nada mais aconterá no projeto
+    setTimeout(() => {
+      this.isFormSend = false;
+    }, 5 * 1000);
+  }
 }
